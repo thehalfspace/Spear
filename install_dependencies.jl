4cee800e4c6b56d9f0d3d99ff75cfbea970364c4
Prithvi Thakur <prithvi.thakur93@gmail.com>
2020-09-19 13:32:41 -0400

    add pardiso libraries

--- install_dependencies.jl
+++ install_dependencies.jl
@@ -1,5 +1,6 @@
 import Pkg
 Pkg.add(["Printf", "LinearAlgebra", "DelimitedFiles", "SparseArrays", 
          "AlgebraicMultigrid","StaticArrays", "IterativeSolvers", 
-         "FEMSparse", "PyPlot", "StatsBase", "LaTeXStrings", "PyCall"])
+         "FEMSparse", "PyPlot", "StatsBase", "LaTeXStrings", "PyCall",
+         "MKLSparse", "Pardiso"])
 
